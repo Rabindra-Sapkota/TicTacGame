03717082229621dfc73ad018c21aac5eba641d30
Rabindra <071bex429/2ioe.edu.np>
2020-01-26 13:48:29 +0545

    Control Board added

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ class Gameboard():
     #dictionary is choosen for gameboard as there as fixed 9 place and
     #it will be easier to work as key value pair
     def __init__(self):
-        self.game_board = {1:'',2:'',3:'',4:'',5;'',6:'',7:'',8:'',9:''}
+        self.game_board = {1:' ',2:' ',3:' ',4:' ',5;' ',6:' ',7:' ',8:' ',9:' '}
 
 
     #create set_item function which sets value to users choice position in gameboard
@@ -22,11 +22,11 @@ class Gameboard():
 
     #function to clear gameboard at start/restart of game
     def clear_board(self):
-        self.game_board = {1:'',2:'',3:'',4:'',5;'',6:'',7:'',8:'',9:''}
+        self.game_board = {1:' ',2:' ',3:' ',4:' ',5;' ',6:' ',7:' ',8:' ',9:' '}
 
     #function to validate if desired place is already taken
     def is_place_taken(sef,game_board,position):
-        if game_board[position] != '':
+        if game_board[position] != ' ':
             return True
         else:
             return False
@@ -35,8 +35,8 @@ class Gameboard():
     #it checks every values. false is returned is any of the place is vaccant
     #if no vaccant place is found then loop terminates and true is returned
     def is_board_full(self,game_board):
-        for user_values in game_board.values():
-            if user_values == '':
+        for board_value in game_board.values():
+            if board_value == ' ':
                 return False
         return  True
 
@@ -45,5 +45,98 @@ class Gameboard():
     def is_game_won(self,game_board):
         win_conditions = ((1,2,3),(4,5,6),(7,8,9),(1,4,7),(2,5,8),(3,6,9),(1,5,9),(3,5,7))
         for win_condition in win_conditions:
-            if game_board[win_condition[0]] == game_board[win_condition[1]] == game_board[win_condition[2]] and game_board[win_condition[0]] != '':
+            if game_board[win_condition[0]] == game_board[win_condition[1]] == game_board[win_condition[2]] and game_board[win_condition[0]] != ' ':
                 return True
+        else:
+            return False
+
+    def print_board(self,game_board):
+        index=1
+        for row in range(0,3):
+            for column in range(0,3):
+                if column != 2:
+                    print(game_board[index],end='|')
+                else:
+                    print(game_board[index])
+                index += 1
+            if row != 2:
+                print('-----')
+
+class Game():
+
+# 1 Game Start
+    def start_game(self):
+        self.control_board = Gameboard()
+        self.game_board = self.control_board.game_board
+        self.playerOne = 'O'
+        self.playerTwo = 'X'
+        print("***************************")
+        print("**  Welcome To X-O Game  **")
+        print("***************************")
+        self.player_one_name=input("Please Enter Player One Name : ")
+        self.player_two_name=input("Please Enter Player Two Name : ")
+        print('\nHere each place in board is representd as 1-9 starting from left along row')
+        self.control_board.print_board(self.game_board)
+        self.turn = 1
+
+#2 Game Restart
+
+    def restart_game(self):
+        self.control_board = Gameboard()
+        self.game_board=self.control_board.game_board
+        self.control_board.print_board(self.game_board)
+        self.turn = 1
+
+#3 Game end and play again
+
+    def end_game(self):
+        if self.game_running == False:
+            try:
+                replay = int(input('Press 0 to quit or 1 to play again:'))
+                if (replay != 0 and replay != 1):
+                    raise Exception
+                elif replay == 1:
+                    self.game_running = True
+                    self.restart_game()
+        except:
+            self.end_game()
+
+#3 Game Turn
+
+    def take_turn(self,user,item):
+        try:
+            position = int(input(user + ' choose a place, 1-9 : '))
+            if position > 9 or position < 1:
+                raise Exception
+        except:
+            print("Pick Number between 1-9 : ")
+            return self.take_turn(user,item)
+
+        if self.controlBoard.is_place_taken(self.game_board,position):
+            print("This place is taken")
+            self.take_turn(user,item)
+        else:
+            self.controlBoard.set_items(item,position,self.game_board)
+            self.controlBoard.print_board(self.game_board)
+
+        if self.controlBoard.is_game_won(self.game_board):
+            print(user + " Wins.")
+            self.game_running = False
+
+#4 Game Mamager
+    def main(self):
+        self.game_running = True
+        self.start_game()
+        while self.game_running:
+            if self.turn % 2 != 0:
+                self.take_turn(self.player_one,'O')
+            else:
+                self.take_turn(self.player_two,'X')
+
+            if self.controlBoard.is_board_full(self.game_board):
+                print("Game Draw")
+                self.game_running = False
+            self.turn += 1
+
+            if not self.game_running:
+                self.end_game()
